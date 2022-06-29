1264bdd91e6a420b2811cbfef995188c475b9bab
🍟Joey🍟 <19317273+JWW127@users.noreply.github.com>
2022-06-28 22:42:21 -0500

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Learn Sbux</title>
+        <meta name="learn sbux workflow" content="sbux training app" />
+      </Head>
+
+      <main>
+        <h1 className="text-3xl">hello world</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
