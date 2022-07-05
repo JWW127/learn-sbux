58ecbd4f991eeb368b188c25fbfdf8cb38e42fa5
🍟Joey🍟 <19317273+JWW127@users.noreply.github.com>
2022-07-04 20:18:00 -0500

    order box menu styles

--- src/components/OrderBoxMenu.tsx
+++ src/components/OrderBoxMenu.tsx
@@ -1,6 +1,6 @@
 export const OrderBoxMenu = () => {
   return (
-    <div className="flex flex-wrap font-bold bg-stone-500 h-2/5">
+    <div className="flex flex-wrap font-semibold bg-stone-500 h-2/5">
       <div className="flex flex-wrap mt-2 basis-full">
         <button className="w-[6vw] basis-1/5 h-1/6 p-1 text-sm shadow-inner shadow-lg shadow-gray-800 bg-orange-300 active:bg-green-700 active:shadow-gray-900 rounded-[50%] ">
           Quantity
@@ -15,27 +15,27 @@ export const OrderBoxMenu = () => {
         <div className="self-center pr-2 text-2xl text-right text-white basis-3/4">
           <p>Total Due:</p>
         </div>
-        <button className="basis-1/4 w-[6vw] bg-stone-700 rounded-md text-white">
+        <button className="basis-1/4 w-[6vw] bg-stone-700 rounded-md text-white shadow-inner drop-shadow-xl shadow-stone-500 active:bg-stone-800">
           {false ? `total` : `$0.00`}
         </button>
         <div className="flex flex-wrap justify-between pt-2 gap-2 basis-full h-1/2">
           {" "}
-          <button className="w-[10vw] bg-red-500 rounded-md shadow-lg text-white shadow-inner active:bg-red-600">
+          <button className="w-[10vw] bg-red-500 rounded-md text-white active:bg-red-600 drop-shadow-xl shadow-stone-500 shadow-inner">
             void item
           </button>
-          <button className="w-[10vw] rounded-md bg-sky-100 shadow-lg">
+          <button className="w-[10vw] rounded-md bg-sky-100 drop-shadow-xl shadow-stone-500 shadow-inner active:bg-sky-200">
             For here
           </button>
-          <button className="w-[10vw] rounded-md bg-green-200 shadow-lg">
+          <button className="w-[10vw] rounded-md bg-green-200 drop-shadow-xl shadow-stone-500 shadow-inner active:bg-green-300">
             Discount
           </button>
-          <button className="w-[4vw] mx-[3vw] bg-red-400 rounded-full shadow-md shadow-stone-500 shadow-inner">
+          <button className="w-[4vw] mx-[3vw] bg-red-400 rounded-[50%] drop-shadow-xl shadow-stone-500 shadow-inner active:bg-red-500">
             Lock
           </button>
-          <button className="w-[10vw] rounded-md bg-sky-100 shadow-lg">
+          <button className="w-[10vw] rounded-md bg-sky-100 drop-shadow-xl shadow-stone-500 shadow-inner active:bg-sky-200">
             To Go
           </button>
-          <button className="w-[10vw] text-white rounded-md bg-green-600 shadow-lg">
+          <button className="w-[10vw] text-white rounded-md bg-green-600 drop-shadow-xl shadow-stone-500 shadow-inner active:bg-green-700">
             Find Order
           </button>
         </div>
--- src/components/PosTopNav.tsx
+++ src/components/PosTopNav.tsx
@@ -1,6 +1,6 @@
 export const PosTopNav = () => {
   return (
-    <div className="h-[10vh] basis-full bg-stone-500 flex justify-around items-center text-xl">
+    <div className="h-[10vh] basis-full bg-stone-500 flex justify-around items-center text-lg">
       <button className="w-[10vw] h-[80%] shadow-inner shadow-lg shadow-stone-900  bg-stone-800 active:bg-stone-900 active:shadow-stone-400 rounded-xl text-white">
         Order
       </button>
@@ -10,19 +10,19 @@ export const PosTopNav = () => {
       <button className="w-[10vw] h-[80%] shadow-inner shadow-lg shadow-stone-900  bg-stone-800 active:bg-stone-900 active:shadow-stone-400 rounded-xl text-white">
         Functions
       </button>
-      <button className="w-[12vw] h-[70%]  shadow-inner shadow-lg shadow-gray-800 bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%] ">
+      <button className="w-[12vw] h-[70%]  shadow-inner shadow-gray-800 drop-shadow-xl bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%]">
         Misc
       </button>
-      <button className="w-[12vw] h-[70%]  shadow-inner shadow-lg shadow-gray-800 bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%] ">
+      <button className="w-[12vw] h-[70%]  shadow-inner shadow-gray-800 drop-shadow-xl bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%]">
         Beans
       </button>
-      <button className="w-[12vw] h-[70%]  shadow-inner shadow-lg shadow-gray-800 bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%] ">
+      <button className="w-[12vw] h-[70%]  shadow-inner shadow-gray-800 drop-shadow-xl bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%]">
         RTD&E
       </button>
-      <button className="w-[12vw] h-[70%]  shadow-inner shadow-lg shadow-gray-800 bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%] ">
+      <button className="w-[12vw] h-[70%]  shadow-inner shadow-gray-800 drop-shadow-xl bg-green-600 active:bg-green-700 active:shadow-gray-900 rounded-[50%]">
         Food
       </button>
-      <button className="w-[12vw] h-[70%]  shadow-inner shadow-lg shadow-gray-800 bg-stone-200 active:bg-stone-400 active:shadow-gray-900 rounded-[50%] ">
+      <button className="w-[12vw] h-[70%]  shadow-inner shadow-gray-800 drop-shadow-xl bg-stone-200 active:bg-stone-400 active:shadow-gray-900 rounded-[50%]">
         Drink
       </button>
     </div>
