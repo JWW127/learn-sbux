09b41aaa9e5a7df7595f3ea2965b603246c8a66a
🍟Joey🍟 <19317273+JWW127@users.noreply.github.com>
2022-07-25 23:52:52 -0500

    lots of button refactoring into seperate components

--- src/components/DrinkSizeMenu.tsx
+++ src/components/DrinkSizeMenu.tsx
@@ -1,131 +1,49 @@
-import { BtnComponent, OffBtnComponent } from "./MenuButtons";
+import {
+  GreenBtn,
+  OffBtnComponent,
+  OrangeBtn,
+  PurpleBtn,
+  RoseBtn,
+  TealBtn,
+  YellowBtn,
+} from "./MenuButtons";
 
 export const DrinkSizeMenu = () => {
   return (
     <div className="h-full basis-[85%] py-4 pr-4 bg-white flex flex-col">
       <div className="flex flex-wrap justify-around pt-5 pb-14 gap-y-6 gap-x-1 basis-2/5">
-        <BtnComponent
-          design="bg-teal-500 border-2 border-gray-900 rounded-lg shadow-lg shadow-inner shadow-stone-200 active:shadow-stone-900 active:bg-teal-600 basis-1/6"
-          label="btn"
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-orange-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-orange-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <BtnComponent
-          design="bg-orange-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-orange-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
+        <TealBtn label="btn" />
+        <OffBtnComponent label="" />
+        <YellowBtn label="btn" />
+        <OrangeBtn label="btn" />
+        <OrangeBtn label="btn" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OrangeBtn label="btn" />
+        <OrangeBtn label="btn" />
       </div>
       <div className="flex flex-wrap justify-around gap-y-6 gap-x-1 basis-3/5">
-        <BtnComponent
-          design="bg-purple-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-purple-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-purple-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-purple-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-purple-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-orange-400 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-orange-400 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <BtnComponent
-          design="bg-orange-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <OffBtnComponent
-          design="border-gray-900 rounded-lg basis-1/6"
-          label=""
-        />
-        <BtnComponent
-          design="bg-green-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-green-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-green-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
-        <BtnComponent
-          design="bg-yellow-200 border-2 border-gray-900 rounded-lg basis-1/6"
-          label="btn"
-        />
+        <PurpleBtn label="btn" />
+        <PurpleBtn label="btn" />
+        <PurpleBtn label="btn" />
+        <PurpleBtn label="btn" />
+        <PurpleBtn label="btn" />
+        <RoseBtn label="btn" />
+        <RoseBtn label="btn" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OrangeBtn label="btn" />
+        <YellowBtn label="btn" />
+        <OffBtnComponent label="" />
+        <GreenBtn label="btn" />
+        <GreenBtn label="btn" />
+        <GreenBtn label="btn" />
+        <YellowBtn label="btn" />
+        <YellowBtn label="btn" />
+        <YellowBtn label="btn" />
+        <YellowBtn label="btn" />
+        <YellowBtn label="btn" />
       </div>
     </div>
   );
--- src/components/MenuButtons.tsx
+++ src/components/MenuButtons.tsx
@@ -1,16 +1,61 @@
 interface BtnProps {
   label: string;
-  design: string;
+  design?: string;
 }
 
-export const BtnComponent = (props: BtnProps) => {
-  return <button className={props.design}>{props.label}</button>;
-};
-
+//These buttons are only to be used with the main menu like DrinkSizeMenu, BlendedMenu, EspMenu etc. cannot be used with nav or menubox
 export const OffBtnComponent = (props: BtnProps) => {
   return (
-    <button disabled={true} className={props.design}>
+    <button disabled={true} className="border-gray-900 rounded-lg basis-1/6">
       {props.label}
     </button>
   );
 };
+
+export const PurpleBtn = (props: BtnProps) => {
+  return (
+    <button className="bg-purple-200 border-2 border-gray-900 rounded-lg shadow-lg shadow-inner shadow-stone-200 active:shadow-stone-900 active:bg-purple-400 basis-1/6">
+      {props.label}
+    </button>
+  );
+};
+export const RoseBtn = (props: BtnProps) => {
+  return (
+    <button className="border-2 border-gray-900 rounded-lg shadow-lg shadow-inner bg-rose-300 shadow-stone-200 active:shadow-stone-900 active:bg-rose-400 basis-1/6">
+      {props.label}
+    </button>
+  );
+};
+
+export const YellowBtn = (props: BtnProps) => {
+  return (
+    <button className="bg-yellow-200 border-2 border-gray-900 rounded-lg shadow-lg shadow-inner shadow-stone-200 active:shadow-stone-900 active:bg-yellow-400 basis-1/6">
+      {props.label}
+    </button>
+  );
+};
+
+export const GreenBtn = (props: BtnProps) => {
+  return (
+    <button className="bg-green-200 border-2 border-gray-900 rounded-lg shadow-lg shadow-inner shadow-stone-200 active:shadow-stone-900 active:bg-green-300 basis-1/6">
+      {props.label}
+    </button>
+  );
+};
+
+export const OrangeBtn = (props: BtnProps) => {
+  return (
+    <button className="bg-orange-200 border-2 border-gray-900 rounded-lg shadow-lg shadow-inner shadow-stone-200 active:shadow-stone-900 active:bg-orange-400 basis-1/6">
+      {props.label}
+    </button>
+  );
+};
+
+export const TealBtn = (props: BtnProps) => {
+  return (
+    <button className="bg-teal-500 border-2 border-gray-900 rounded-lg shadow-lg shadow-inner shadow-stone-200 active:shadow-stone-900 active:bg-teal-600 basis-1/6">
+      {props.label}
+    </button>
+  );
+};
+
