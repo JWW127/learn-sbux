96bfeed14be61f041f42eae8488d2bd20459e57c
🍟Joey🍟 <19317273+JWW127@users.noreply.github.com>
2022-07-31 22:10:08 -0500

    tea menu layout

--- src/components/TeaMenu.tsx
+++ src/components/TeaMenu.tsx
@@ -1,6 +1,56 @@
+import {
+  PurpleBtn,
+  YellowBtn,
+  OrangeBtn,
+  RoseBtn,
+  SkyBtn,
+  LightAmberBtn,
+  OffBtnComponent,
+  BlackBtn,
+  GreenBtn,
+} from "./MenuButtons";
+
 export const TeaMenu = () => {
   return (
-      <div className="h-full basis-[85%] bg-white"><h1>Tea Menu</h1></div>
+    <div className="h-full flex flex-wrap basis-[85%] bg-white">
+      {" "}
+      <div className="flex flex-wrap justify-around py-1 text-sm gap-x-1 gap-y-4">
+        <YellowBtn label="Chai" />
+        <GreenBtn label="Matcha"/>
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <SkyBtn label="Black" />
+        <YellowBtn label="BLK Lemonade" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <SkyBtn label="Green" />
+        <YellowBtn label="GRN Lemonade" />
+        <OffBtnComponent label="" />
+        <SkyBtn label="Peach Green" />
+        <YellowBtn label="PGRN LMNDE" />
+        <SkyBtn label="Passion" />
+        <YellowBtn label="PSN Lemonade" />
+        <OffBtnComponent label="" />
+        <SkyBtn label="Guava Black" />
+        <YellowBtn label="GBLK LMNDE" />
+        <SkyBtn label="Custom" />
+        <YellowBtn label="Custom LMNDE" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <SkyBtn label="IceT Refill" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <OffBtnComponent label="" />
+        <BlackBtn label="Brewed" />
+      </div>
+    </div>
   );
 };
-
